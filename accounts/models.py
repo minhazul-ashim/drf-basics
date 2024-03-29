e65962111f88ffab6bc89333d226054a46cd748d
Minhazul Ashim <ashim.ashim7470@gmail.com>
2024-03-29 08:37:28 +0600

    adding authentication

--- accounts/models.py
+++ accounts/models.py
@@ -7,5 +7,5 @@ class UserAccount(models.Model) :
     accType = models.CharField(max_length = 10);
     location = models.CharField(max_length = 100, null=True);
     phone = models.CharField(max_length = 20, null=True);
-    upload = models.FileField(upload_to="uploads/", null=True, blank=True)
+    resume = models.FileField(upload_to="uploads/", null=True, blank=True)
     
